24896d4a9d8e9033ba6f5789b3bc4fb6bee90c2b
andreishishvatov <andreishishvatov@gmail.com>
2019-04-20 17:05:59 +0400

    apple spawn function is redady

--- script.js
+++ script.js
@@ -1,7 +1,36 @@
-const apple = () => {
-    const x = Math.floor(Math.random() * 13)
-    const y = Math.floor(Math.random() * 13)
-    console.log(`${x}; ${y}`)
+let apple = {}
+
+const spawnApple = () => {
+    let check = []
+    let x = Math.floor(Math.random() * 13 + 1) 
+    let y = Math.floor(Math.random() * 13 + 1)
+    let id = `${x}; ${y}` 
+    if (id === '7; 7') {
+        check.push(true)
+    }
+    else {
+        check.push(false)
+    }
+    while (check[0]) {
+    x = Math.floor(Math.random() * 13 + 1) 
+    y = Math.floor(Math.random() * 13 + 1)
+    id = `${x}; ${y}`
+    if (id != '7; 7') {
+        check.pop()
+        check.push(false)
+    }
+    }
+    apple.xCoordinate = x
+    apple.yCoordinate = y 
+    const randomTile = document.getElementById(id)
+    randomTile.style.backgroundColor = "red"
+
 }
 
-apple()
+let snake = {}
+
+const spawnSnake = () => {
+
+}
+
+spawnApple()
