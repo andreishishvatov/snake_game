d6e3e7482159835570d0fcc7f10640d7001c2eca
andreishishvatov <andreishishvatov@gmail.com>
2019-04-20 18:01:51 +0400

    moveSnakeR in process

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 let apple = {}
 
+let snake = {
+    strlocation: '7; 7',
+    xCoordinate: 7,
+    yCoordinate: 7,
+    direction: 'R',
+    length: 4,
+}
+
+const convertCoordinates = (xC, yC) => {
+    return `${yC}; ${xC}`
+}
+
 const spawnApple = () => {
     let check = []
     let x = Math.floor(Math.random() * 13 + 1) 
     let y = Math.floor(Math.random() * 13 + 1)
     let id = `${x}; ${y}` 
-    if (id === '7; 7') {
+    if (id === '7; 7' || id === '7; 6' || id === '7; 5' || id === '7; 4') {
         check.push(true)
     }
     else {
@@ -15,7 +27,7 @@ const spawnApple = () => {
     x = Math.floor(Math.random() * 13 + 1) 
     y = Math.floor(Math.random() * 13 + 1)
     id = `${x}; ${y}`
-    if (id != '7; 7') {
+    if (id != '7; 7' || id === '7; 6' || id === '7; 5' || id === '7; 4') {
         check.pop()
         check.push(false)
     }
@@ -27,10 +39,21 @@ const spawnApple = () => {
 
 }
 
-let snake = {}
-
 const spawnSnake = () => {
+    document.getElementById("7; 7").style.backgroundColor = 'blue'
+    document.getElementById("7; 6").style.backgroundColor = 'blue'
+    document.getElementById("7; 5").style.backgroundColor = 'blue'
+    document.getElementById("7; 4").style.backgroundColor = 'blue'
+}
 
+function moveSnakeR () {
+    document.getElementById(snake.strlocation).style.backgroundColor = 'green'
+    snake.xCoordinate = snake.xCoordinate + 1
+    snake.strlocation = convertCoordinates(snake.xCoordinate, snake.yCoordinate)
+    document.getElementById(snake.strlocation).style.backgroundColor = "blue"
 }
 
 spawnApple()
+spawnSnake()
+
+setTimeout(moveSnakeR, 1000)
